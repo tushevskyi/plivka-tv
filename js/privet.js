c684b2481ceaae84f00bc43d5374b435955526d7
Bogdan Tushevskyi <tushevskuy@gmail.com>
2017-07-04 17:53:54 +0300

    add about text markup changes

--- js/privet.js
+++ js/privet.js
@@ -157,17 +157,16 @@ $(function() {
   $(player).one('play', soundFadeOut);
 
   function soundFadeOut() {
-    let _volumeInterval = setInterval(volumeUp, 350),
-        volume          = 0;
+    // let _volumeInterval = setInterval(volumeUp, 350),
+    //     volume          = 0;
       
-    function volumeUp() {
-      volume += 0.05;
-      if(volume > 1) {
-        clearInterval(_volumeInterval);
-      }
-      player.volume = volume.toFixed(2);
-    }
-
+    // function volumeUp() {
+    //   volume += 0.05;
+    //   if(volume > 1) {
+    //     clearInterval(_volumeInterval);
+    //   }
+    //   player.volume = volume.toFixed(2);
+    // }
   }
 
   let quality_holder      = $('.quality__holder'),
@@ -184,7 +183,6 @@ $(function() {
   function changeQuality(e) {
     console.log(videoObj);
     console.log("player: " + player.currentTime);
-    // hideBttnChoosedElement(e,img_quality_holder);
 
     let video_currentTime = player.currentTime || videoObj.current.start_time,
         fullUrl           = "";
